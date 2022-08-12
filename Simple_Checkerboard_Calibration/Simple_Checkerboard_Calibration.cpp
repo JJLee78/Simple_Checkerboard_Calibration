0d4a5186715f2ef6054a5db872e84112403f0af1
Jaejun Lee <jaejun.lees@gmail.com>
2022-08-12 16:08:10 +0900

    커밋 및 푸시 테스트

--- Simple_Checkerboard_Calibration/Simple_Checkerboard_Calibration.cpp
+++ Simple_Checkerboard_Calibration/Simple_Checkerboard_Calibration.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
 #include <opencv2/core.hpp>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+
+using namespace std;
+
+#define PATTERN_NUM      (10)   // # Input 이미지 갯수
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+    return 0;
 }
